9daca767c174ed29b7faf1a6d8fdf81b63dbd4ac
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-10-24 12:55:39 +0200

    add speaker and feedback service

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+const router = express.Router();
+
+module.exports = (params) => {
+  router.get('/', (request, response) => {
+    if (!request.session.visitcount) {
+      request.session.visitcount = 0;
+    }
+    request.session.visitcount += 1;
+    console.log(`number of visitors: ${request.session.visitcount}`);
+
+    response.render('pages/index', { pageTitle: 'Welcome' });
+  });
+
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
+  return router;
+};
+
+/*
+
+
+app.get('/', (request, response) => {
+  response.render('pages/index', { pageTitle: 'Welcome'});
+});
+
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+*/
