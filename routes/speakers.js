cddd5390ece66cd5f4466bab5c7789984d42a333
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-10-26 13:41:07 +0100

    chapter 4 f

--- routes/speakers.js
+++ routes/speakers.js
@@ -7,14 +7,14 @@ module.exports = params => {
 
   router.get('/', async (request, response) => {
     const speakers = await speakersService.getList();
-    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
+    const artwork = await speakersService.getAllArtwork();
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers, artwork });
   });
 
   router.get('/:shortname', async (request, response) => {
     const speaker = await speakersService.getSpeaker(request.params.shortname);
-    const speakerArtwork = await speakersService.getArtworkForSpeaker(request.params.shortname);
-    console.log(speakerArtwork);
-    response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker, speakerArtwork });
+    const artwork = await speakersService.getArtworkForSpeaker(request.params.shortname);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker, artwork });
   });
 
 
